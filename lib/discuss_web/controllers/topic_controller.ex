dd758a97a12d13b8f9d98c2188c44723b217d926
danilomartins <dglmartins@outlook.com>
2018-05-29 23:31:44 -0400

    only owner can edit, update and delete topics - plug and html hide

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -3,8 +3,11 @@ defmodule DiscussWeb.TopicController do
 
   alias Discuss.Forum
   alias Discuss.Forum.Topic
+  alias Discuss.Repo
 
   plug DiscussWeb.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+
+  plug :check_topic_owner when action in [:update, :edit, :delete]
 
   def index(conn, _params) do
     IO.inspect(conn.assigns)
@@ -54,6 +57,18 @@ defmodule DiscussWeb.TopicController do
     conn
     |> put_flash(:info, "Topic deleted successfully.")
     |> redirect(to: topic_path(conn, :index))
+  end
+
+  defp check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You cannot edit that")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
   end
 
 end
